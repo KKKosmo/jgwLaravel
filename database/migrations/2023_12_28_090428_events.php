dabbba7f9d08d39c465a36870aeb0c84cb6ffd70
manil <manilalouisangel2@gmail.com>
2024-03-10 19:08:41 +0800

    can delete records by removing foreign key relationship

--- database/migrations/2023_12_28_090428_events.php
+++ database/migrations/2023_12_28_090428_events.php
@@ -8,19 +8,18 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('Events', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('record_id');
             $table->string('type', 20)->nullable();
             $table->string('summary', 500)->nullable();
             $table->string('user', 20);
-            $table->foreign('record_id')->references('id')->on('main');
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('Events');
+        Schema::dropIfExists('events');
     }
 };
